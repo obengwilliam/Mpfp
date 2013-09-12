94b80c90f87c481150406a8bfc87e181e3064e09
william <obengwilliam>
2013-09-12 20:51:06 +0000

    a

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,3 @@
- 
-    $('#contacts').on('click', function () {
-	    
-	    var section = $(this).parent();
-		
-		section.toggle();
-	    section.siblings(".slide").slideToggle('2000', "easeInQuart");
-	});
+$('.carousel').carousel({
+  interval: 2000
+})
